f9b59ee56e65355cc466a0fbf1409e1fcff453b0
Shotic <115079622+Shotic@users.noreply.github.com>
2023-09-10 18:56:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import streamlit as st
 from PIL import Image
+from streamlit_extras.let_it_rain import rain
+
+st.set_page_config(page_title="Miss na kita", page_icon="😓", layout="wide")
+rain(
+    emoji="💞",
+    font_size=20,
+    falling_speed=5,
+    animation_length="infinite",
+)
 
 # assets 
 img_contact_f = Image.open("images/1.jpg")
 
 img_contact_r = Image.open("images/2.jpg")
 
-
-st.set_page_config(page_title="Miss na kita", page_icon="😓", layout="wide")
+img_contact_g = Image.open("images/3.jpg")
 
 st.subheader("Hi, miss na miss na kita ay hayayayyayayyayayyaaa")
 
@@ -110,6 +118,7 @@ with st.container():
     """
     )
     
+    
 with st.container():
     st.write("---")
     st.header("Number 1 bb niya: ")
@@ -154,8 +163,73 @@ with st.container():
     """
     )
     
+with st.container():
+    st.write("---")
+    st.header("Love story: ")
+    st.write("##")
+    image_d, text_f = st.columns((4, 2))
+    with text_f:
+        st.subheader("Kelan ba nagumpisa yung pagmamahal ko sa bbq kamo?")
+        st.write(
+        """
+        Bali nagumpisa kong nalaman na crush ko pala bbq nung Feb wuntik na nga ako nag confess nung valentines day sayang....,Nagumpisa palang nung unang beses ko palang siya narinig na kumanta sa isang project nag karoon na pala ako ng pagkahanga sa bbq di ko lang narealize ito kasi slow ko eh sorry ba
+        tas nagdevelop nalang tong feeling ni hotdog seller(ako oo na ako na hotdog seller😂😂😂) lalo na nung panahon ng meron kami practice ng Contemp Dance (WHAT IF WE REWRITE THE STARS🎶🎶🎶) mas napa enjoy ako pumasok kasi dito sa point na to siya na lagi kong kasama umuwi first tawag
+        ko chikahan kaya kami nag sasabay pero sa totoo lang gusto ko lang talaga makasama bbq (oo bakit ba pakipot ako) bali isang araw nag practice kami sa san fab beach di ako pinapansin ng bbq (Luh Ka jan boy lagoat) tas narinig ko nalang na may sinabi si stella na "Ayy may inaupdate yan"
+        nung narinig ko yun parang nagiba yung mundo ko nun, habang papauwi na kami nila angeloph napaisip nalang ako sa jeep na "hmmmmm may inaaupdate siya.........naunahan ako......." bali sa time na yun akala ko na yung bbq meron ng iba na kausap kaya mejo nag reready na ako na mag move on
+        kasi akala ko meron na tas pagkatapos nun, nag sasama parin kami sa pag uwi ng bbq pagtapos ng school kaya mejo nalito ako ng unte...........after mga ilang days malapit na ako makamove on until na nagsama kami sa mcdo kumain...... sinabi niya na "May aaminin ako sayo" napa bilis ng tibok puso ko nun
+        kala ko kung ano na yun pala sasabihin niya lang na may ibang may crush sakin di siya, di.....yung bbq yung may crush sakin tinanong niya ako "Yun lang reaction mo dun?" napa sagot ako ng "Ano ba dapat reaction ko dun?" kasi yung inaasahan ko sana na ikaw yung may crush sakin bbq eh.......
+        maya maya nag uusap na tayo malapit na tayo mag punta kila seth inaantay natin si Xandra tas may nabangit ka na wala na kayo ni J (di ko paren kilala kung sino si j na yun pake ko dun🤬🤬🤬🤬🤬) bali nung narinig ko yun bumalik sa dati feeling ko inisip ko na "May chance ako"
+        mga ilang araw nakalipas natapos na ang contemp ilang araw at gabi ko na iniisip na kung may chance pa ba talaga ako kay bbq..............ilang gabi.........at araw kong inisip......kahit tapos na contemp tayo parin nagsasabay na umuwi nun at sa pagkakataon naging mag ka groupo uli tayo sabi ko na
+        "ikaw nanaman ka grupo ko" pero sa totoo lang masaya ako nun kasi naging ka grupo ko yung bbq at makakasama ko siya.......makalipas ng mga ilang araw dumating ang araw na April 16 6:27 PM may chat ako sa bbq na "shaine meron akong gusto sabihin pero ang random, so bali eto nagkaroon ako ng feelings para saiyo pero di ko alam gagawin,
+        iignore ko lang ba?, palipasin lang?, ayaw ko naman na masira yung friendship naten bali sinasabi ko lang ito para malaman mo....." pinatay ko lahat ng devices ko mapa laptop man o desktop naka patay.......makalipas ng isang minuto....dumating ang 6:28 sinabi naman ng bbq "omg ka heuheueheuu hinihintay lang
+        kita magconfess ehh"...........nagloadingako............loading........tas nag click sakin na nakapag confess ako sa bbq......nakayanan ko..........ngiting tagumpay si hotdog seller.....ngiting hangang tenga.......(kinikilig ako habang sinuisulat to).......nakalipas ang mga ilang araw mas naging close tayo
+        at lagi na rin umuuwi magkasama.......takas doon....takas ditooo......at dun nag umpisa ang pagmamahalan namin ng bbq.........hanggang sa naging ngaun na..........💞💞💞💞
+        """
+        )
+    with image_d:
+        st.image(img_contact_g)
+
+with st.container():
+    st.write("---")
+    st.header("Mga nagustahan ko sa bbq")
+    st.write("##")
+    st.write(
+    """
+    -Mga nagustuhan ko sa mahiwagang bbq:
+    
+    - Napakaganda ng ngiti
+    
+    - Napakabait
+    
+    - Napakaganda ng mata
+    
+    - Napakagandang kausap
+    
+    - Napakakyut ng boses
+    
+    - Napakaganda
+    
+    - Napaka kyut
+    
+    - Napaka galing kumanta
+    
+    - Napaka talentado
+    
+    - Napaka sweet
+    
+    - Napakamaunawain
+    
+    - Napakamaasikaso
+    
+    Madami pa yan di heh
+    
+    """
+    )
+
+st.write("---")
+
 st.subheader("The end")
 
 st.title("MORE:")
-
+st.write("##")
 st.write("Gusto ko lang sabihin matagal na nung nakailang araw poa na pero ang hirap sabihin na ano na nga yun basta ano uli tawag dun hmmmmmm....... waiit iisipin ko pa..........ahhh yun alam ko na..... miss na kita")
